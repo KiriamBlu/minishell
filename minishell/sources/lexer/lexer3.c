885fa0f75ac2f4c8fbeb4ef884f256c2c79b819a
Joan Sanfelipe escamilla <jsanfeli@c3r2s3.42madrid.com>
2022-03-14 15:50:43 +0100

    EXPANSION WORKS

--- minishell/sources/lexer/lexer3.c
+++ minishell/sources/lexer/lexer3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 00:21:27 by jporta            #+#    #+#             */
-/*   Updated: 2022/03/02 03:06:49 by jporta           ###   ########.fr       */
+/*   Updated: 2022/03/14 15:49:55 by jsanfeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,50 +52,42 @@ int ft_getlenname(char *line, int start)//mirar si start es == a dollar o el ant
 	i = start + 1;
 	if(line[i] == '?')
 		return(2);
-	while(ft_isalnum(line[i]) == 1 && line[i] != ' ' && line[i] != '$' && line[i] != '"')
+	while(ft_isalnum(line[i]) == 1 && line[i + 1] != ' ' && line[i + 1] != '$' && line[i + 1] != '"')
 		i++;
 	return(i - start);
 }
 
-char *expanddollar(char *line, int start, t_list *list)
+char *expanddollar(char *name, t_list *list)
 {
-	char	*name;
 	char	*expand;
 	int		check;
 
-	name = ft_substr(line, start + 1, ft_getlenname(line, start));
 	if(name[0] == '?')
 	{
 		free(name);
 		return("$");
 	}
 	check = getposinlst(list, name) + 1;
-	free(name);
-	if(check == -1)
-	{
-		free(name);
+	if(check == 0)
 		return(" ");
-	}
 	expand = getlineinenv(list, check);
-	return(ft_substr(expand, ft_getlenname(line, start), ft_strlen(expand)));
+	return(ft_substr(expand, ft_strlen(name) + 1, ft_strlen(expand)));
 }
 
-char *ft_prueba(char *line, t_list *list)
+char *ft_prueba(char *line, t_list *list) //NAME[0] = FULL; NAME[1] = TMP; NAME[2] = AUX; NAME[3] = NAME
 {
 	int	i;
 	int	a;
 	int k;
-	char *full;
-	char *tmp;
-	char *aux;
+	char *name[4];
 	
 	i = 0;
 	a = 0;
 	k = countdollars(line);
 	if(k == 0)
 		return(ft_strdup(line));
-	tmp = ft_strdup(line);
-	full = ft_calloc(sizeof(char), 1);
+	name[1] = ft_strdup(line); //GUARDAR EN LA TMP LA LINEA PARA EMPEZAR A TRABAJAR CON UNA LINEA CAMBIANTE
+	name[0] = ft_calloc(sizeof(char), 1); //DAR UNA PEQUEÑA RESERVA INCIAL PARA PODER UNIRLE MAS PARTES DE LA LINEA PARA SER DEVUELTA
 	while (k > 0)
 	{
 		while (line[a] != '$')
@@ -105,18 +97,19 @@ char *ft_prueba(char *line, t_list *list)
 					;
 			a++;
 		}
-		aux = ft_substr(line, i, a);
-		aux = ft_strjoin(aux, expanddollar(line, a, list));
-		full = ft_strjoin(full, aux);
-		free(aux);
-		a += ft_getlenname(line, a);
-		free(tmp);
-		tmp = ft_substr(line, a, full - line);
+		name[2] = ft_substr(line, i, a - i); //GUARDAS DESE EL PUUNTO DONDE VAS A TRABAJAR EN AUX PARA AÑADIR DESDE AHI LA EXPANSIÓN
+		name[3] = ft_substr(line, a + 1, ft_getlenname(line, a)); //OBTIENES Y ALMACENAS EL NOMBRE DE LA VARIABLE A SER EXPANDIDA
+		name[2] = ft_strjoin(name[2], expanddollar(name[3], list)); //UNES LA EXPANSION DESDE EL TAMAÑO DE REYENO HASTA EL FINAL DE AUX
+		name[0] = ft_strjoin(name[0], name[2]);//HAS UNIDO LA EXPANSIÓN AL FULL
+		free(name[2]);
+		a += ft_strlen(name[3]) + 1; //GUARDAS LA POSICION DESDE DONDE CUENTAS
+		free(name[1]);
+		name[1] = ft_substr(line, a, name[0] - line); //EN LA TEMPORAL GUARDAS EL FINAL
 		i = a;
 		k--;
 	}
-
-	full = ft_strjoin(full, tmp);
-	
-	return (full);
+	name[0] = ft_strjoin(name[0], name[1]); //EL RESTO DE LA FRASE UNA VEZ NO QUEDAN '$'
+	free(name[1]);
+	//system("leaks minishell");
+	return (name[0]);
 }
